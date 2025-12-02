3fd6b8447895942738c4388072baf37018481229
s1018812 <s1018812@student.knoxschools.org>
2025-12-02 21:11:34 +0000

    H

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-#Hello
-#This is sams change
-#This is harshita
+# Most streamed spotify songs 2023
+import pandas as pd
+df = pd.read_csv("/content/sample_data/california_housing_train.csv")
+
+class data_set: 
+    def __init__(): 
+
