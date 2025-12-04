3db9f3acf24e3ed6b34ee9451873dc11bfc82faf
s1018812 <s1018812@student.knoxschools.org>
2025-12-04 15:57:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # Most streamed spotify songs 2023
 import pandas as pd
-df = pd.read_csv("/content/sample_data/california_housing_train.csv")
+df = pd.read_csv("/workspaces/Coding2-PANDAS-Project/spotify-2023.csv")
 
 class data_set: 
-    def __init__(): 
+    def __init__(self, set): 
+        self.data_set = set
+
+    def clean_data(self): 
+        self.data_set.pop("in_apple_playlists")
+        self.data_set.pop("in_apple_charts")
+        self.data_set.pop("in_deezer_playlists")
+        self.data_set.pop("in_deezer_charts")
+        self.data_set.pop("in_shazam_charts")
+        self.data_set.pop("key")
+        self.data_set.pop("mode")
+    
+    def recommend_song(self, artisit, value): 
+
+    def most_popular_song(self, artist): lkk
+
 
