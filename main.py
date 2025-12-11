1fd4ef1ad7e1948b7911adb6e58d942a399e4e3f
s1018812 <s1018812@student.knoxschools.org>
2025-12-11 16:30:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Most streamed spotify songs 2023
 import pandas as pd
-df = pd.read_csv("/workspaces/Coding2-PANDAS-Project/spotify-2023.csv")
+df = pd.read_csv("/workspaces/Coding2-PANDAS-Project/spotify-2023-clean.csv")
 
 class data_set: 
     def __init__(self, set): 
@@ -15,11 +15,20 @@ class data_set:
         self.data_set.pop("key")
         self.data_set.pop("mode")
     
-    def recommend_song(self, artisit, value): # by danceabiltiy etc. 
-        recommended = df.sort_values(by = '')
+    def recommend_song(self, artist, value): # by danceabiltiy etc. 
+        artist_sorted = self.data_set.loc[self.data_set["artist(s)_name"] == artist]
+        recommended = artist_sorted.sort_values(by = value, ascending = False)
+        return recommended.head(1)     
 
-    def most_popular_song(self, artist): # artist most popular song in 2023
+    def most_popular_song(self, artist, value): # artist most popular song in 2023 
+    # In UI, pass streams to value when calling this function
+        artist_sorted = self.data_set.loc[self.data_set["artist(s)_name"] == artist]
+        most_popular = artist_sorted.sort_values(by = value, ascending = False)
 
-    def mini_playlist(self, song): 
-    # make playlist based off song given by user and finding songs with similar descriptive values; like 5 songs
+    # def mini_playlist(self, song): 
+    # # make playlist based off song given by user and finding songs with similar descriptive values; like 5 songs
 
+
+harshita = data_set(df)
+harshita.clean_data()
+harshita.recommend_song("Taylor Swift", "acousticness_%")
