faf35a345fae028e0e9c599f9200ebefb04fb482
s1018812 <s1018812@student.knoxschools.org>
2025-12-16 13:16:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 import pandas as pd
 df = pd.read_csv("/workspaces/Coding2-PANDAS-Project/spotify-2023-clean.csv")
 
+#Create class for the spotify data set
 class data_set: 
+    # Initialize data set
     def __init__(self, set): 
         self.data_set = set
 
+    # Remove unecessary columns from the data set
     def clean_data(self): 
         self.data_set.pop("in_apple_playlists")
         self.data_set.pop("in_apple_charts")
@@ -15,16 +18,19 @@ class data_set:
         self.data_set.pop("key")
         self.data_set.pop("mode")
     
+    # Have user input an artist and song value (ex.danceabiltiy) and recommennd song by artist with the highest score for that value
     def recommend_song(self, artist, value): # by danceabiltiy etc. 
         artist_sorted = self.data_set.loc[self.data_set["artist(s)_name"] == artist]
         recommended = artist_sorted.sort_values(by = value, ascending = False)
         return recommended.head(1)     
 
+    # Return artist's most popular song
     def most_popular_song(self, artist, value): # artist most popular song in 2023 
     # In UI, pass streams to value when calling this function
         artist_sorted = self.data_set.loc[self.data_set["artist(s)_name"] == artist]
         most_popular = artist_sorted.sort_values(by = value, ascending = False)
 
+    # Create a mini playlist for the user based on the values of a song that the user inputted
     def mini_playlist(self, song): 
         index = int((df.loc[df['track_name'] == song]).index[0])
         acousticness = df.loc[index]["acousticness_%"]
@@ -62,14 +68,7 @@ class data_set:
         return playlist
         
         ## TO DO: 
-        # Harshita: Make 6 different loops for each quality
         # Sam: Create UI
-        # Make Video on Monday N3
-
-
-
-    # # make playlist based off song given by user and finding songs with similar descriptive values; like 5 songs
-
 
 harshita = data_set(df)
 harshita.clean_data()
